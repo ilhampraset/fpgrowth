13a86a24e03c3ce8462f2c1d6c80415ff53bcba3
ilhamekaprasetya <ilham.eka@majoo.id>
2023-12-05 13:33:40 +0700

    chore: add streamlit

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
+import string
 import time
 
 from mlxtend.frequent_patterns import fpgrowth, association_rules
 import pandas as pd
+from fpgrowth_py import fpgrowth as fpg2
+from fpgrowth_py.utils import *
+import streamlit as st
 from mlxtend.preprocessing import TransactionEncoder
 
-if __name__ == '__main__':
-    url = "https://docs.google.com/spreadsheets/d/1VqTaGSr5vGu8qSwk2O0IIIjsPMCFyVvS/gviz/tq?tqx=out:csv&sheet=Sheet1"
+
+def to_frozenset(x):
+    return frozenset(map(any, x.split("{")[1].split("}")[0].split(",")))
+
+
+def to_string_format(x):
+    return "->".join(x)
+
+
+def execfpgrowth(min_support, min_confidence):
+    url = ("https://docs.google.com/spreadsheets/d/1VqTaGSr5vGu8qSwk2O0IIIjsPMCFyVvS/gviz/tq?tqx=out:csv&sheet"
+           "=Sheet1")
     groceries = pd.read_csv(url)
     transactions = groceries['barang'].apply(lambda t: t.split(', '))
 
@@ -16,13 +30,23 @@ if __name__ == '__main__':
     te_ary = te.fit(transactions).transform(transactions)
     pd.set_option('display.max_rows', None)
     pd.set_option('display.width', None)
+
     df = pd.DataFrame(te_ary, columns=te.columns_)
-    result = fpgrowth(df, min_support=0.1, use_colnames=True, verbose=0)
-    rules = association_rules(result, metric="confidence", min_threshold=0.5)
-    print(result)
-    print(rules)
+    result = fpgrowth(df, min_support=min_support, use_colnames=True, verbose=1)
+    rules = association_rules(result, metric="confidence", min_threshold=min_confidence)
+    result['itemsets'] = result['itemsets'].apply(lambda x: to_string_format(x))
+    rules['antecedents'] = rules['antecedents'].apply(lambda x: to_string_format(x))
+    rules['consequents'] = rules['consequents'].apply(lambda x: to_string_format(x))
+
+    st.write(result)
+    st.write(rules)
     end = time.time()
     print("time execution : %f" % (end - start))
 
 
+if __name__ == '__main__':
+    min_support = st.number_input('Insert Minimum Support')
+    min_confidence = st.number_input('Insert Minimum Confidence')
+    if st.button('Process'):
+        execfpgrowth(min_support, min_confidence)
 
