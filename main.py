fbcb953d0f71d5737b4c64b6cf5b53d65ca9466d
ilhamekaprasetya <ilhampraset30@gmail.com>
2024-01-26 15:14:14 +0700

    feat:csv dataset

--- main.py
+++ main.py
@@ -1,70 +1,59 @@
-import json
 import math
-import string
 import time
 import pyfpgrowth
-from mlxtend.frequent_patterns import fpgrowth, association_rules
 import pandas as pd
 import fptree
-import fp
 import streamlit as st
 from mlxtend.preprocessing import TransactionEncoder
-
-
-def to_frozenset(x):
-    return frozenset(map(any, x.split("{")[1].split("}")[0].split(",")))
 
 
 def frozenset_to_str(fset):
     return ", ".join(sorted(list(fset)))
 
 
-def to_string_format(x):
-    return "->".join(x)
-
-
 def execfpgrowth(min_support, min_confidence):
-    url = ("https://docs.google.com/spreadsheets/d/1VqTaGSr5vGu8qSwk2O0IIIjsPMCFyVvS/gviz/tq?tqx=out:csv&sheet"
-           "=Sheet1")
-    groceries = pd.read_csv(url)
+
+    groceries = pd.read_csv('dataset.csv')
     transactions = groceries['barang'].apply(lambda t: t.split(', '))
-    min_support = math.floor(min_support * len(transactions))
-    print(min_support)
+    msupport = math.floor(min_support * len(transactions))
+    print(msupport)
     transactions = list(transactions)
     te = TransactionEncoder()
     te_ary = te.fit(transactions).transform(transactions)
     df = pd.DataFrame(te_ary, columns=te.columns_)
-    print(df)
-    st.table(df)
-    transformed_data = {frozenset(item): 1 for item in transactions}
 
+    st.dataframe(df, hide_index=True)
     start = time.time()
 
-    fp_tree, header_table = fptree.build_fp_tree(transactions, min_support)
-    print(transactions)
+    fp_tree, header_table = fptree.build_fp_tree(transactions, msupport)
 
     js = list(fptree.fp_tree_to_json(fp_tree).items())
     st.title("FP Tree")
     st.json(js[2][1])
-    patterns = pyfpgrowth.find_frequent_patterns(transactions, min_support)
-
+    patterns = pyfpgrowth.find_frequent_patterns(transactions, msupport)
     rules2 = pyfpgrowth.generate_association_rules(patterns, min_confidence)
     table_data = []
+    count = 0
     for consequent, (antecedent, confidence) in rules2.items():
         antecedent_str = frozenset_to_str(antecedent)
         consequent_str = frozenset_to_str(consequent)
-        support = patterns[consequent]
-        lift_ratio = confidence/support
+        supports = patterns[consequent] / len(transactions)
+        lift_ratio = confidence / support
+        count = count + 1
         table_data.append(
-            {'Rule': antecedent_str+' => '+consequent_str, 'Support': support/len(transactions), 'Confidence': confidence, 'Lift Ratio':lift_ratio})
+            {'No': count, 'Rule': antecedent_str + ' => ' + consequent_str, 'Support': supports,
+             'Confidence': confidence,
+             'Lift Ratio': lift_ratio})
+
     st.title("Association Rules")
-    st.table(table_data)
+
+    st.dataframe(table_data, hide_index=True)
     end = time.time()
     print("time execution : %f" % (end - start))
 
 
 if __name__ == '__main__':
-    min_support = st.number_input('Insert Minimum Support')
-    min_confidence = st.number_input('Insert Minimum Confidence')
+    support = st.number_input('Insert Minimum Support')
+    confidences = st.number_input('Insert Minimum Confidence')
     if st.button('Process'):
-        execfpgrowth(min_support, min_confidence)
+        execfpgrowth(support, confidences)
